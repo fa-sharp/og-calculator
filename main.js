19923f5924a77bdb5b17d0c503fb1d7c59419a35
farshad <farshadpiano@gmail.com>
2021-07-28 01:12:09 -0400

    Added operation display

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { operate } from './operations.js';
 
-const display = document.querySelector(".display")
+const resultDisplay = document.querySelector("#resultDisplay");
+const opDisplay = document.querySelector("#opDisplay");
+
 const numButtons = document.querySelectorAll(".buttons>.num");
 const opButtons = document.querySelectorAll(".buttons>.op");
 const clearButton = document.querySelector("#clear");
@@ -10,8 +12,18 @@ const clearButton = document.querySelector("#clear");
 numButtons.forEach(numButton => {
     const num = numButton.textContent;
 
-    numButton.addEventListener("click", () => display.textContent = display.textContent += num);
+    numButton.addEventListener('click', () => resultDisplay.textContent = resultDisplay.textContent += num);
+});
+
+/** OPERATION BUTTONS */
+opButtons.forEach(opButton => {
+    const op = opButton.textContent;
+
+    opButton.addEventListener('click', () => opDisplay.textContent = op);
 })
 
 /** CLEAR BUTTON */
-clearButton.addEventListener("click", () => display.textContent = '');
+clearButton.addEventListener('click', () => {
+    resultDisplay.textContent = '';
+    opDisplay.textContent = '';
+});
